006c12a214dc2d2812dac2eaf7decdabb7acdf28
Samed Yalniz <s.yalniz@hotmail.de>
2016-06-26 12:07:04 +0200

    Exercise 8.1

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 
-students = [
+studentss = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
   {name: "Nurse Ratched", cohort: :november},
@@ -20,16 +20,31 @@ puts "--------------"
 end 
 
 def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort"
+	students.each_with_index do |student,index|
+		no = index+1 
+		puts "#{no}.#{student[:name]} (#{student[:cohort]} cohort"
 	end 
 end 
 
 def print_footer(names)
 	puts "Overall, we have #{names.count} great students"
 end 
+
+def input_students 
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	students = []
+	name = gets.chomp
+	while !name.empty? do 
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.count} students"
+		name = gets.chomp
+	end 
+	students 
+end 
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
-
 
