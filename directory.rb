b58abdb73758741e4e240b1de697cb6a4ab03cdc
Samed Yalniz <s.yalniz@hotmail.de>
2016-06-27 09:41:45 +0200

    everything inclusive 8.6

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def print_header 
-puts "The students of villains Academy"
-puts "--------------"
+puts "The students of villains Academy".center(50)
+puts "--------------".center(50)
 end 
 
 def print(students)
@@ -11,39 +11,69 @@ def print(students)
 end 
 
 def print_footer(names)
-	puts "Overall, we have #{names.count} great students"
+	if names.count == 1 
+		puts "Overall, we have #{names.count} great student".center(50)
+	else 
+		puts "Overall, we have #{names.count} great students".center(50)
+	end 
 end 
 
 def input_students 
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	puts "Please enter the names of the students".center(50)
+	puts "To finish, just hit return twice".center(50)
 	students = []
 	name = gets.chomp
+	puts "Please enter the cohort".center(50)
+	month = gets.chomp
+	puts "Please enter students hobbies".center(50)
+	hobbies = gets.chomp
+	puts "Please enter the country of birth of the student".center(50)
+	country = gets.chomp
+	puts "Please enter the students' height".center(50)
+	height = gets.chomp
 	while !name.empty? do 
-		students << {name: name, cohort: :november}
-		puts "Now we have #{students.count} students"
+
+		students << {name: name, cohort: month, hobbies: hobbies, country: country, height: height}
+		if students.count == 1 
+			puts "Now we have #{students.count} student".center(50)
+		else 
+			puts "Now we have #{students.count} students".center(50)
+		end 
+		puts "Please enter the names of more students".center(50)
+		puts "To finish, just hit return twice".center(50)
 		name = gets.chomp
+		if !name.empty? 
+			puts "Please enter the cohort".center(50)
+			month = gets.chomp
+			puts "Please enter students hobbies".center(50)
+			hobbies = gets.chomp
+			puts "Please enter the country of birth of the student".center(50)
+			country = gets.chomp
+			puts "Please enter the students' height".center(50)
+			height = gets.chomp
+		end 
 	end 
 	students 
+
 end 
 
 students = input_students
 def print_specific_letter(students)
-	puts "Please enter a specific letter to print out all names starting with it"
+	puts "Please enter a specific letter to print out all names starting with it".center(20)
 	letter = gets.chomp
 	students.each do |student|
 		if student[:name].downcase.chr == letter
-			puts "#{student[:name]}"
+			puts "#{student[:name]}".center(50)
 		end 
 	end 
 	puts ""
 end  
 
 def print_short(students)
-	puts "Printing students with names shorter than 12 chars"
+	puts "Printing students with names shorter than 12 chars".center(50)
 	students.each do |student|
 		if student[:name].length < 12
-			puts "#{student[:name]}"
+			puts "#{student[:name]}".center(50)
 
 		end 
 	end 
@@ -52,7 +82,7 @@ end
 def print_with_loop (students)
 	counter = 0
 	while counter != students.length
-		 puts "#{(students[counter])[:name]} #{(students[counter])[:cohort] cohort}"
+		 puts "#{(students[counter])[:name]} #{(students[counter])[:cohort]} cohort #{(students[counter])[:hobbies]} #{(students[counter])[:country]} #{(students[counter])[:height]}".center(50)
 		 counter +=1 
 	end 
 end 
